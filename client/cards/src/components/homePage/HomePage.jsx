36d2cc1a4557c0d5cf9a4ed9ab85e0572c4f8985
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-10-16 20:13:12 +0300

    Create new changes realted with home page field

--- client/cards/src/components/homePage/HomePage.jsx
+++ client/cards/src/components/homePage/HomePage.jsx
@@ -33,7 +33,7 @@ export const HomePage = () => {
                             <h1>Imagine a place...</h1>
                             <p>...where you can wish a loved one everything you feel for them</p>
                             <div className={styles}>
-                                {/* <button className={styles.shopNowButton}>SHOP NOW</button> */}
+                                <button className={styles.shopNowButton} onClick={goToShop}>SHOP NOW</button>
                                 <button className={styles.aboutButton}>ABOUT</button>
                             </div>
                         </div>
@@ -57,6 +57,24 @@ export const HomePage = () => {
                         </div>
                         <div className={styles.bannerImage}>
                             <img className={styles.animatedBox} src='../../../images/home-page/animatetBox.gif' alt='animatedBox' />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div className={styles.articleBanner}>
+                <section className={styles.firstSection}>
+                    <div className={styles.bannerContainerOne}>
+                        <div className={styles.bannerContent}>
+                            <h1>Have you ever wondered...</h1>
+                            <p>... hmm what would be the most appropriate wish?</p>
+                            <div className={styles}>
+                                <button className={styles.shopNowButton} onClick={goToShop}>SHOP NOW</button>
+                                <button className={styles.aboutButton}>ABOUT</button>
+                            </div>
+                        </div>
+                        <div className={styles.bannerImage}>
+                            <img className={styles.imageOne} src='../../../images/home-page/image2.png' alt='greetingCartOne' />
                         </div>
                     </div>
                 </section>
