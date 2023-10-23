96cefff778ec273c6ebee2480e5fa7362744a17b
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-10-23 16:03:11 +0300

    Delete some unused code in cards f

--- client/cards/src/utils/Cards/CatalogCards/Cards.jsx
+++ client/cards/src/utils/Cards/CatalogCards/Cards.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { checkForDiscount } from '../../../functions/checkForDiscount';
 import { addToOrder } from '../../../functions/addToOrder';
