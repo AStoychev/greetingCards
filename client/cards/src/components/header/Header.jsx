afe2b3c68ee3b9c35b8f23c78eeca59986943205
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-10-27 15:48:53 +0300

    Changes related with style in Header field and react icon install

--- client/cards/src/components/header/Header.jsx
+++ client/cards/src/components/header/Header.jsx
@@ -14,12 +14,16 @@ import { addToOrder } from '../../functions/addToOrder';
 // import Cookies from 'universal-cookie';
 
 // import { AiFillHeart } from 'react-icons/ai'
+import { FaBars } from "react-icons/fa";
 import styles from "./Header.module.css"
 
 export const Header = () => {
     const { isAuthenticated, userId, userEmail, userName } = useContext(AuthContext);
 
     const [logoutModal, setLogoutModal] = useState();
+    const [mobileNav, setMobileNav] = useState(0);
+    const [colorMobileNav, setColorMobileNav] = useState('white')
+    const [dropdownMobileNav, setDropdownMobileNav] = useState();
 
     const navigate = useNavigate();
 
@@ -34,6 +38,45 @@ export const Header = () => {
 
     const showLogoutModal = () => {
         setLogoutModal(<ShowLogoutModal onLoadLogoutModal={onLoadLogoutModal} />)
+    }
+
+    const closeMobileNav = () => {
+        setColorMobileNav('white')
+        setDropdownMobileNav()
+    }
+
+    const clickMObileNavButton = () => {
+        let openCloseMobileNav = mobileNav + 1
+        if (openCloseMobileNav % 2 == 1) {
+            setColorMobileNav('ae0303')
+            setDropdownMobileNav(
+                <div className={styles.containerMobileNav}>
+                    <Link className={styles.mobileNavigationLink} to="/">Home</Link>
+                    <Link className={styles.mobileNavigationLink} to="/catalog">Catalog</Link>
+                    {!isAuthenticated &&
+                        <>
+                            <Link className={styles.mobileNavigationLink} to="/login">Login</Link>
+                            <Link className={styles.mobileNavigationLink} to="/register">Register</Link>
+                        </>
+                    }
+
+                    {isAuthenticated &&
+                        <>
+                            <Link className={styles.mobileNavigationLink} to="/add-card">Add Card</Link>
+                            <Link className={styles.mobileNavigationLink} to={`/admin-main-page/${userId}`}>Admin</Link>
+                            <button className={styles.mobileNavigationLinkButton} onClick={showLogoutModal}>Logout</button>
+                            {/* <Link className={styles.navigationLink} onClick={showLogoutModal} to="/logout">Logout</Link> */}
+                            <div className={styles.mobileUserName}>{userName}</div>
+                        </>
+                    }
+                    <div className={styles.closeMobileNavBottom} onClick={closeMobileNav}>...</div>
+                </div>
+            )
+        } else {
+            setColorMobileNav('white')
+            setDropdownMobileNav()
+        }
+        setMobileNav(openCloseMobileNav)
     }
 
     // if (isAuthenticated) {
@@ -100,8 +143,17 @@ export const Header = () => {
                 </div>
 
                 <div className={styles.basket}>
-                    <Basket />
+                    <div className={styles.basketWrapper}>
+                        <Basket/>
+                    </div>
+                    <div className={styles.mobileNavigation}>
+                        <button onClick={clickMObileNavButton}>
+                            <FaBars className={styles.mobileIcon} style={{ color: `${colorMobileNav}` }} />
+                        </button>
+                        {dropdownMobileNav}
+                    </div>
                 </div>
+
             </div >
 
         </header>
