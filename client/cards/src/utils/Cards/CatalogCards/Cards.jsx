9292fe46eedcaef06d9b5dbc2d14ca24d8384504
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-10-28 18:53:15 +0300

    Some changes in cards field

--- client/cards/src/utils/Cards/CatalogCards/Cards.jsx
+++ client/cards/src/utils/Cards/CatalogCards/Cards.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { checkForDiscount } from '../../../functions/checkForDiscount';
-import { addToOrder } from '../../../functions/addToOrder';
 
 import styles from './Cards.module.css'
 
