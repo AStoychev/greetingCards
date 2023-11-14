8d181bea42d760be76a320e92ad589669c571868
Stoychev <stoychev.nas@gmail.com>
2023-11-14 18:02:02 +0200

    Design changes - mobile responsive addCard, CardDetails, Header, Line, SecodStepOrder, ThirdStepOrder fields

--- client/cards/src/components/cardDetails/CardDetails.jsx
+++ client/cards/src/components/cardDetails/CardDetails.jsx
@@ -122,7 +122,9 @@ export const CardDetails = () => {
         <div>
             <div className={styles.container}>
                 {deleteModal}
-                <h2>Details</h2>
+                <div className={styles.header}>
+                    <h2>Details</h2>
+                </div>
                 <div className={styles.columns3}>
 
                     <div className={styles.columnsLeft}>
