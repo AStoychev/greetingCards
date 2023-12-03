28d107809b831a42aac60326ec13f0319dafb33e
Stoychev <stoychev.nas@gmail.com>
2023-12-03 17:21:53 +0200

    Create design changes in first filed in chat bot and clickable button on chat when click over button open the chat when click again chat is close

--- client/cards/src/components/chat/Chat.jsx
+++ client/cards/src/components/chat/Chat.jsx
@@ -7,6 +7,7 @@ import styles from "./Chat.module.css"
 
 export const Chat = () => {
 
+    const [open, setOpen] = useState(0);
     const [openChat, setOpenChat] = useState('')
 
     const closeChat = () => {
@@ -14,8 +15,15 @@ export const Chat = () => {
     }
 
     const openChatField = () => {
-        setOpenChat(<ChatWindow closeChat={closeChat}/>)
+        if(open === 0) {
+            setOpen(1);
+            setOpenChat(<ChatWindow closeChat={closeChat}/>);
+        } else {
+            setOpen(0);
+            closeChat();
+        }
     }
+
     return (
         <div className={styles.container}>
             <div className={styles.chatWrapper}>
