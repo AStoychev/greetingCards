02655f4423d1b0b5ebe9e70890848aa9a09256ab
Stoychev <stoychev.nas@gmail.com>
2023-12-09 19:26:24 +0200

    Create design in edit card page use desig from add card

--- client/cards/src/components/editCard/editCard.jsx
+++ client/cards/src/components/editCard/editCard.jsx
@@ -7,6 +7,9 @@ import { useCardContext } from "../../contexts/CardContext";
 import { useService } from "../../hooks/useService";
 import { cardServiceFactory } from "../../services/cardService";
 
+import background from '../img/background.png'
+import styles from "../addCard/AddCard.module.css"
+
 export const EditCard = () => {
 
     const { onCardEditSubmit } = useCardContext();
@@ -43,114 +46,139 @@ export const EditCard = () => {
 
     // Empty input string solve idea
     const checkForEmptyInput = (data) => {
-        if(!data) {
+        if (!data) {
             return data = '';
         } else {
             return data;
         }
     }
 
+    const validateImageUrl = (url) => {
+        let regex = /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+        return regex.test(url)
+    }
+
+    const validateAddCardFileds = () => {
+        if (values.title.length >= 2
+            &&
+            values.description.length >= 10
+            &&
+            values.price > 0
+            &&
+            validateImageUrl(values.imageUrl)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
     return (
-        <div className="container">
-            <div>
-                <h1>Edit Card</h1>
-                <div>
-                    <section id="card-page" className="content auth">
-                        <form id="card" method='POST' onSubmit={onSubmit}>
-                            <div className='field-card'>
+        <div className={styles.container} style={{ backgroundImage: `url(${background})` }}>
+            <div className={styles.innerContainer}>
+                <div className={styles.mainField}>
+                    <h1>Edit Card</h1>
+                    <div className={styles.innerColumn}>
+                        <section id="card-page" className="content auth">
+                            <form id="card" method='POST' onSubmit={onSubmit}>
+                                <div className={styles.fieldCard}>
 
-                                <label className='htmlContent' htmlFor="title">Title:</label>
-                                <input
-                                    type="text"
-                                    id="title"
-                                    name="title"
-                                    placeholder="Title"
-                                    value={values.title}
-                                    onChange={changeHandler}
-                                />
+                                    <label className={styles.labelContent} htmlFor="title">Title:<span>*</span></label>
+                                    <input
+                                        type="text"
+                                        id="title"
+                                        name="title"
+                                        placeholder="Title"
+                                        value={values.title}
+                                        onChange={changeHandler}
+                                    />
 
-                                <label className='htmlContent' htmlFor="description">Description:</label>
-                                <input
-                                    type="text"
-                                    id="description"
-                                    name="description"
-                                    placeholder="Description"
-                                    value={values.description}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="price">Price:</label>
-                                <input
-                                    type="number"
-                                    name="price"
-                                    id="price"
-                                    placeholder="0.00"
-                                    value={values.price}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="discount">Discount:</label>
-                                <input
-                                    type="number"
-                                    name="discount"
-                                    id="discount"
-                                    placeholder="0%"
-                                    value={checkForEmptyInput(values.discount)}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="imageUrl">ImageURL:</label>
-                                <input
-                                    type="url"
-                                    name="imageUrl"
-                                    id="imageUrl"
-                                    placeholder="https//"
-                                    value={values.imageUrl}
-                                    onChange={changeHandler}
-                                />
-                                {/* Additional Image */}
-                                <label className='htmlContent' htmlFor="additionalImageOne">Aditional Image One:</label>
-                                <input
-                                    type="url"
-                                    name="additionalImageOne"
-                                    id="additionalImageOne"
-                                    placeholder="https//"
-                                    value={checkForEmptyInput(values.additionalImageOne)}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="additionalImageTwo">Aditional Image Two:</label>
-                                <input
-                                    type="url"
-                                    name="additionalImageTwo"
-                                    id="additionalImageTwo"
-                                    placeholder="https//"
-                                    value={checkForEmptyInput(values.additionalImageTwo)}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="additionalImageThree">Aditional Image Three:</label>
-                                <input
-                                    type="url"
-                                    name="additionalImageThree"
-                                    id="additionalImageThree"
-                                    placeholder="https//"
-                                    value={checkForEmptyInput(values.additionalImageThree)}
-                                    onChange={changeHandler}
-                                />
-                                <label className='htmlContent' htmlFor="additionalImageFour">Aditional Image Four:</label>
-                                <input
-                                    type="url"
-                                    name="additionalImageFour"
-                                    id="additionalImageFour"
-                                    placeholder="https//"
-                                    value={checkForEmptyInput(values.additionalImageFour)}
-                                    onChange={changeHandler}
-                                />
-                                {/* Additional Image */}
-                            </div>
-                            <div className="submit">
-                                <input className="submit" type="submit" value="Create" onClick={setDiscount} style={{ marginTop: "21px" }} />
-                            </div>
-                        </form>
-                    </section>
+                                    <label className={styles.labelContent} htmlFor="description">Description:<span>*</span></label>
+                                    <input
+                                        type="text"
+                                        id="description"
+                                        name="description"
+                                        placeholder="Description"
+                                        value={values.description}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="price">Price:<span>*</span></label>
+                                    <input
+                                        type="number"
+                                        name="price"
+                                        id="price"
+                                        placeholder="0.00"
+                                        value={values.price}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="discount">Discount:</label>
+                                    <input
+                                        type="number"
+                                        name="discount"
+                                        id="discount"
+                                        placeholder="0%"
+                                        value={checkForEmptyInput(values.discount)}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="imageUrl">ImageURL:<span>*</span></label>
+                                    <input
+                                        type="url"
+                                        name="imageUrl"
+                                        id="imageUrl"
+                                        placeholder="https//"
+                                        value={values.imageUrl}
+                                        onChange={changeHandler}
+                                    />
+                                    {/* Additional Image */}
+                                    <label className={styles.labelContent} htmlFor="additionalImageOne">Aditional Image One:</label>
+                                    <input
+                                        type="url"
+                                        name="additionalImageOne"
+                                        id="additionalImageOne"
+                                        placeholder="https//"
+                                        value={checkForEmptyInput(values.additionalImageOne)}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="additionalImageTwo">Aditional Image Two:</label>
+                                    <input
+                                        type="url"
+                                        name="additionalImageTwo"
+                                        id="additionalImageTwo"
+                                        placeholder="https//"
+                                        value={checkForEmptyInput(values.additionalImageTwo)}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="additionalImageThree">Aditional Image Three:</label>
+                                    <input
+                                        type="url"
+                                        name="additionalImageThree"
+                                        id="additionalImageThree"
+                                        placeholder="https//"
+                                        value={checkForEmptyInput(values.additionalImageThree)}
+                                        onChange={changeHandler}
+                                    />
+                                    <label className={styles.labelContent} htmlFor="additionalImageFour">Aditional Image Four:</label>
+                                    <input
+                                        type="url"
+                                        name="additionalImageFour"
+                                        id="additionalImageFour"
+                                        placeholder="https//"
+                                        value={checkForEmptyInput(values.additionalImageFour)}
+                                        onChange={changeHandler}
+                                    />
+                                    {/* Additional Image */}
+                                </div>
+                                <div className="submit">
+                                    <input
+                                    className={!validateAddCardFileds() ? styles.disabledSubmitBtn : styles.submitBtn}
+                                    type="submit"
+                                    value="Edit"
+                                    onClick={setDiscount}
+                                    />
+                                </div>
+                            </form>
+                        </section>
+                    </div>
                 </div>
-
             </div>
 
         </div>
