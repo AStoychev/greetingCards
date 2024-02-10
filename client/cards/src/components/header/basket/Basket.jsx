bf55caeb335c3b2f99f1ac939971f5a4aa973645
Stoychev <stoychev.nas@gmail.com>
2024-02-10 18:37:07 +0200

    Change purchase modal design, burrons, functionality, delete some unused code field

--- client/cards/src/components/header/basket/Basket.jsx
+++ client/cards/src/components/header/basket/Basket.jsx
@@ -1,62 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { BasketModal } from '../../../utils/Modals/basketModal/BasketModal';
 
-import { addToOrder } from '../../../functions/addToOrder';
-
+import { TiShoppingCart } from "react-icons/ti";
 import styles from './Basket.module.css'
 
 export const Basket = () => {
 
-    const [purchase, setPurchase] = useState(0);
     const [purchaseModal, setPurchaseModal] = useState([]);
 
     const onPurchseModal = (data) => {
         if (data === 'Close') {
             setPurchaseModal([])
-            setPurchase(0)
         }
-    };
-
-    const showOrder = () => {
-        // setPurchaseModal(<BasketModal onPurchseModal={onPurchseModal} />)
-        setPurchase(1)
-    };
-
-    const closeOrder = () => {
-        // setPurchaseModal([]);
-        setPurchase(0)
     };
 
     const onBascketClick = () => {
         setPurchaseModal(<BasketModal onPurchseModal={onPurchseModal} />)
-        setPurchase(1)
     };
-
 
     // Try to close order on click outside order field
     const onClickOutSide = (e) => {
         if (purchaseModal != false && e.target.closest("#mainPopup") === null) {
             setPurchaseModal(false)
-            setPurchase(0)
+
         }
         // console.log(e.target.closest('#mainPopup'));
     }
 
     return (
         <div className={styles.mainRight} onClick={onClickOutSide}>
-            <div className={styles.basketMain} onMouseEnter={showOrder} onMouseLeave={closeOrder} onClick={onBascketClick}>
-            {/* <div className={styles.basketMain} onMouseEnter={showOrder} onMouseLeave={closeOrder} onClick={onBascketClick}> */}
+            <div className={styles.basketMain} onClick={onBascketClick}>
+                {/* <div className={styles.basketMain} onMouseEnter={showOrder} onMouseLeave={closeOrder} onClick={onBascketClick}> */}
                 {purchaseModal}
                 <div className={styles.innerBascketOne}>
-                    {
-                        purchase === 0
-                            ?
-                            <img className={styles.bascketLogo} src="../../../images/purchase.png" alt="purchase" />
-                            :
-                            <img className={styles.bascketLogo} src="../../../images/purchaseHover.png" alt="purchase" />
-                    }
-                    {/* <img className={styles.bascketLogo} src="images/purchase.png" alt="purchase" /> */}
+                    <TiShoppingCart className={styles.bascketLogo} />
                 </div>
                 <div className={styles.innerBascketTwo}>
                     {/* {price} */}
--- client/cards/src/utils/Modals/basketModal/BasketModal.jsx
+++ client/cards/src/utils/Modals/basketModal/BasketModal.jsx
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom";
 
 import { MapOrder } from "./MapOrder/MapOrder";
 import { FinalPrice } from "./FinalPrice/FinalPrice";
+import { totalPrice } from "../../../functions/totalPrice";
 
 import { showAllPurchase } from "../../../functions/localStorageFunction/showAllPurchase";
-import { deletePurchase } from "../../../functions/localStorageFunction/deletePurchase";
 
 import styles from "./BasketModal.module.css"
 
@@ -27,7 +27,7 @@ export const BasketModal = ({
         <div className={styles.container} onClick={() => handleClose('Close')}>
             <div className={styles.popup} id="mainPopup" onClick={e => { e.stopPropagation() }}>
                 <div className={styles.wrapper}>
-                    {/* <button className={styles.closeButton} onClick={handleClose}>&times;</button> */}
+                    <button className={styles.closeButton} onClick={() => handleClose('Close')}>&times;</button>
                     <h2>My Order</h2>
 
                     <div className={styles.paragraphCookie}>
@@ -37,17 +37,15 @@ export const BasketModal = ({
                             &&
                             <div className={styles.emptyList}>Not purchase yet!</div>
                         }
-
                     </div>
-
+                    
                     <FinalPrice />
-
                     <div className={styles.buttonsOrder}>
-                        <div className={styles.buttonClose}>
+                        <div className={styles.buttonWrapper}>
                             <button className={styles.bottomButton} onClick={() => handleClose('Close')}>Close</button>
-                        </div>
-                        <div className={styles.buttonOrder}>
-                            <button className={styles.bottomButton} onClick={firsStepOrder}>Order</button>
+                            {parseFloat(totalPrice()) > 0 &&
+                                <button className={styles.bottomButton} onClick={firsStepOrder}>Order</button>
+                            }
                         </div>
                     </div>
 
