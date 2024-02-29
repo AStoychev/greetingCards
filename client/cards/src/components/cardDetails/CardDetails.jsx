5c02f753a50dd8685493ed929ce2a3c57e253cad
Stoychev <stoychev.nas@gmail.com>
2024-02-29 16:52:52 +0200

    Changes related with formatting

--- client/cards/src/components/cardDetails/CardDetails.jsx
+++ client/cards/src/components/cardDetails/CardDetails.jsx
@@ -128,6 +128,10 @@ export const CardDetails = () => {
                 {deleteModal}
 
 
+
+
+
+
                 <div className={styles.header}>
                     <h2>Details</h2>
                 </div>
