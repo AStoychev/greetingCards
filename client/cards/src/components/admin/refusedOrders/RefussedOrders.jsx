53ce78f7360f366312130225d8d820dadef5b3cd
Stoychev <stoychev.nas@gmail.com>
2024-03-06 16:51:46 +0200

    Create reffused orders formating separated to components field

--- client/cards/src/components/admin/refusedOrders/RefussedOrders.jsx
+++ client/cards/src/components/admin/refusedOrders/RefussedOrders.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from 'react';
 
 import { orderServiceFactory } from '../../../services/orderService';
-import { showDateTime } from '../functions/showDateTime';
 import { takeTypeOrder } from '../functions/takeTypeOrder';
 
 import { Pattern } from '../pattern/Pattern';
 import { ModalOrder } from '../modalOrder/ModalOrder';
 import { TableHeading } from '../adminUtils/tableHeading/TableHeading';
+import { ShowOrders } from '../adminUtils/orders/ShowOrders';
 
 import styles from './RefussedOrders.module.css'
 
 export const RefussedOrders = () => {
 
     const [refuseOrders, sestRefusedOrders] = useState([]);
-    const [orders, setOrders] = useState('off');
     const [idOrder, setIdOrder] = useState();
     const [showModal, setShowModal] = useState();
     const allOrdersService = orderServiceFactory();
@@ -43,30 +42,7 @@ export const RefussedOrders = () => {
                 <div className={styles.tableContainer}>
                     <TableHeading />
 
-                    {
-                        refuseOrders.length
-                            ?
-                            refuseOrders.map((x, index) => (
-                                <div className={styles.tableRow} key={x._id}>
-                                    <div className={styles.rowItem}>{index + 1}</div>
-                                    <div className={styles.rowItem}>{x._id}</div>
-                                    <div className={styles.rowItem}>{showDateTime(x.createdAt)[0]}</div>
-                                    <div className={styles.rowItem}>{x.city} {x.address}</div>
-                                    <div className={styles.rowItem}>{x.firstName} {x.lastName}</div>
-                                    <div className={styles.rowItem}>{x.shippingPlace} with {x.shippingCompany}</div>
-                                    <div className={styles.rowItem}>{x.payment}</div>
-                                    <div className={styles.rowItem}>{x.price}</div>
-                                    <div className={styles.rowItem}>{x.orderStatus}</div>
-                                    <div className={styles.rowItemOrder}><button className={styles.buttonSeeItemOrder} onClick={() => showOrder(x._id, x.firstName, x.lastName, x.orders)} value={x._id}>SEE ITEMS</button></div>
-                                    <div className={styles.rowSubContainer}>
-                                        <div className={styles.rowItem}>Sub item 1</div>
-                                        <div className={styles.rowItem}>Sub item 2</div>
-                                    </div>
-                                </div>
-                            ))
-                            :
-                            <div>Not refussed orders</div>
-                    }
+                    <ShowOrders showOrder={showOrder}/>
 
                 </div>
             </div>
