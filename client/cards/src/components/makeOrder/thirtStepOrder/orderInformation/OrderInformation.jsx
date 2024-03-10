38474cb99a80030e72a3b9a0c2623f9ba8960f94
Stoychev <stoychev.nas@gmail.com>
2024-03-10 19:47:23 +0200

    Create separate components for each block of code in ThirdStepOrder

---
+++ client/cards/src/components/makeOrder/thirtStepOrder/orderInformation/OrderInformation.jsx
@@ -0,0 +1,44 @@
+import { showAllPurchase } from '../../../../functions/localStorageFunction/showAllPurchase';
+import { checkForDiscount } from '../../../../functions/checkForDiscount';
+
+import styles from './OrderInformation.module.css';
+
+export const OrderInformation = () => {
+    return (
+        <div>
+            {showAllPurchase().map((x) => (
+                <div className={styles.mainAllPurchase} key={x._id}>
+                    <div className={styles.purchaseTitle}>
+                        <div className={styles.title}>
+                            {x.title}
+                        </div>
+                    </div>
+
+                    <div className={styles.purchaseQuantity}>
+                        <div className={styles.quantity}>
+                            {x.quantity}
+                        </div>
+                    </div>
+
+                    <div className={styles.multiplySign}>
+                        <div className={styles.multiply}>
+                            x
+                        </div>
+                    </div>
+
+                    <div className={styles.purchasePrice}>
+                        <div className={styles.price}>
+                            {checkForDiscount(x.price, x.discount)}
+                        </div>
+                    </div>
+
+                    <div className={styles.purchaseFinalPrice}>
+                        <div className={styles.finalPrice}>
+                            {(x.quantity * (checkForDiscount(x.price, x.discount))).toFixed(2)}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
---
+++ client/cards/src/components/makeOrder/thirtStepOrder/personalData/PersonalData.jsx
@@ -0,0 +1,75 @@
+import { showMakeOrderData } from '../../../../functions/localStorageFunction/showMakeOrderData';
+
+import styles from './PersonalData.module.css';
+
+export const PersonalData = () => {
+    return (
+        <div>
+            {showMakeOrderData().map((x) => (
+                <div className={styles.mainPersonalData} key={x.firstName}>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>FULL NAME:</div>
+                        <div className={styles.detailedData}>{x.firstName} {x.lastName}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>EMAIL:</div>
+                        <div className={styles.detailedData}>{x.email}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>PHONE NUMBER:</div>
+                        <div className={styles.detailedData}>{x.phoneNumber}</div>
+                    </div>
+
+                    <div className={styles.typeSeparator}></div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>SHIPPING COMPANY:</div>
+                        <div className={styles.detailedData}>{x.shippingCompany}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>SHIPPING PLACE:</div>
+                        <div className={styles.detailedData}>{x.shippingPlace}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>CITY:</div>
+                        <div className={styles.detailedData}>{x.city}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>POST CODE:</div>
+                        <div className={styles.detailedData}>{x.postCode}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>ADDRESS:</div>
+                        <div className={styles.detailedData}>{x.address}</div>
+                    </div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>MESSAGE</div>
+                        <div className={styles.detailedData}>{x.takeMessage}</div>
+                    </div>
+
+                    <div className={styles.typeSeparator}></div>
+
+                    <div className={styles.innerPersonalData}>
+                        <div className={styles.typePersonalData}>PAYMENT:</div>
+                        <div className={styles.detailedData}>{x.payment}</div>
+                    </div>
+
+                    <div className={styles.typeSeparator}></div>
+
+                    <div className={styles.innerPrivacyPolicy}>
+                        I am aware of the privacy policy.
+                    </div>
+
+                </div>
+            ))}
+        </div>
+    )
+}
