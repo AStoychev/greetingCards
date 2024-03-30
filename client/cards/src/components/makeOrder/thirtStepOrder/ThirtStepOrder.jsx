0ec1c5305bf800707d7d14abc4f7e8194fd6bba4
Stoychev <stoychev.nas@gmail.com>
2024-03-30 19:44:26 +0200

    Changes related with code format in ThirtStepOrdet in make order

--- client/cards/src/components/makeOrder/thirtStepOrder/ThirtStepOrder.jsx
+++ client/cards/src/components/makeOrder/thirtStepOrder/ThirtStepOrder.jsx
@@ -62,7 +62,6 @@ export const ThirtStepOrder = () => {
                         <OrderInformation />
                         <PurchaseTotalPrice />
                         <BottomButtons onClickSubmit={onClickSubmit}/>
-
                     </div>
                 </div>
 
