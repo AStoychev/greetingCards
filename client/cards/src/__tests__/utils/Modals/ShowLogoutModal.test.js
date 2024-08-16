1736e64f64989b459cdd3cc24067acf593f7d74a
Stoychev <stoychev.nas@gmail.com>
2024-08-16 19:56:45 +0300

    Changes related with add new test for ShowlLogoutModal field

--- client/cards/src/__tests__/utils/Modals/ShowLogoutModal.test.js
+++ client/cards/src/__tests__/utils/Modals/ShowLogoutModal.test.js
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
 import { ShowLogoutModal } from '../../../utils/Modals/logouthModal/ShowLogoutModal';
 
 describe('ShowLogoutModal Component', () => {
@@ -9,8 +8,10 @@ describe('ShowLogoutModal Component', () => {
         expect(screen.getByText('Are you sure you want to logout?')).toBeInTheDocument();
         expect(screen.getByText('No')).toBeInTheDocument();
         expect(screen.getByText('Yes')).toBeInTheDocument();
+
         fireEvent.click(screen.getByText('No'));
         expect(mockOnLoadLogoutModal).toHaveBeenCalledWith('No');
+
         fireEvent.click(screen.getByText('Yes'));
         expect(mockOnLoadLogoutModal).toHaveBeenCalledWith('Yes');
     });
